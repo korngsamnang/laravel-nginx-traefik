253a3e200e5aeab363b5de68e83257344aca9662
Korng Samnang <korngsamnang168@gmail.com>
2025-04-23 12:09:13 +0700

    Update some message

--- app/routes/web.php
+++ app/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "<h1>About Us</h1><p>This is the About page.</p>";
+});
